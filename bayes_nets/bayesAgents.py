16e254a59bab946d9ecde4ab90f4b874b356416a
Chibin Zhang <chibinzhang@hotmail.com>
2020-10-25 17:54:35 +0800

    Finish q1

--- bayes_nets/bayesAgents.py
+++ bayes_nets/bayesAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -75,12 +75,12 @@ def constructBayesNet(gameState):
     - a large number of "observation" variables for each cell Pacman can measure
 
     You *must* name all position and house variables using the constants
-    (X_POS_VAR, FOOD_HOUSE_VAR, etc.) at the top of this file. 
+    (X_POS_VAR, FOOD_HOUSE_VAR, etc.) at the top of this file.
 
     The full set of observation variables can be obtained as follows:
 
         for housePos in gameState.getPossibleHouses():
-            for obsPos in gameState.getHouseWalls(housePos)
+            for obsPos in gameState.getHouseWalls(housePos):
                 obsVar = OBS_VAR_TEMPLATE % obsPos
 
     In this method, you should:
@@ -90,13 +90,27 @@ def constructBayesNet(gameState):
       of possible assignments to `var`. These should again be set using the
       constants defined at the top of this file.
     """
-
     obsVars = []
     edges = []
     variableDomainsDict = {}
 
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    obsVars += [OBS_VAR_TEMPLATE % obsPos for housePos in gameState.getPossibleHouses() for obsPos in gameState.getHouseWalls(housePos)]
+
+    edges += [(pos, house) for pos in [X_POS_VAR, Y_POS_VAR] for house in HOUSE_VARS]
+    edges += [(house, obs) for house in HOUSE_VARS for obs in obsVars]
+
+    variableDomainsDict.update({x : val for x in X_POS_VALS for val in X_POS_VALS})
+    variableDomainsDict.update({y : val for y in Y_POS_VALS for val in Y_POS_VALS})
+    variableDomainsDict.update({house : val for house in HOUSE_VARS for val in HOUSE_VALS})
+    variableDomainsDict.update({obs : val for obs in obsVars for val in OBS_VALS})
+
+    print(obsVars)
+    print(edges)
+    print(variableDomainsDict)
+
+
+    # "*** YOUR CODE HERE ***"
+    # util.raiseNotDefined()
 
     variables = [X_POS_VAR, Y_POS_VAR] + HOUSE_VARS + obsVars
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
@@ -109,7 +123,7 @@ def fillCPTs(bayesNet, gameState):
     fillObsCPT(bayesNet, gameState)
 
 def fillXCPT(bayesNet, gameState):
-    from layout import PROB_FOOD_LEFT 
+    from layout import PROB_FOOD_LEFT
     xFactor = bn.Factor([X_POS_VAR], [], bayesNet.variableDomainsDict())
     xFactor.setProbability({X_POS_VAR: FOOD_LEFT_VAL}, PROB_FOOD_LEFT)
     xFactor.setProbability({X_POS_VAR: GHOST_LEFT_VAL}, 1 - PROB_FOOD_LEFT)
@@ -254,7 +268,7 @@ class BayesAgent(game.Agent):
         evidence = self.getEvidence(gameState)
         unknownVars = [o for o in self.obsVars if o not in evidence]
         eliminationOrder = unknownVars + [X_POS_VAR, Y_POS_VAR, GHOST_HOUSE_VAR]
-        bestFoodAssignment = getMostLikelyFoodHousePosition(evidence, 
+        bestFoodAssignment = getMostLikelyFoodHousePosition(evidence,
                 self.bayesNet, eliminationOrder)
 
         tx, ty = dict(
@@ -315,7 +329,7 @@ class VPIAgent(BayesAgent):
                 (TOP_RIGHT_VAL, TOP_LEFT_VAL)]:
 
             condEvidence = dict(evidence)
-            condEvidence.update({FOOD_HOUSE_VAR: foodHouseVal, 
+            condEvidence.update({FOOD_HOUSE_VAR: foodHouseVal,
                 GHOST_HOUSE_VAR: ghostHouseVal})
             assignmentProb = houseMarginals.getProbability(condEvidence)
 
@@ -384,7 +398,7 @@ class VPIAgent(BayesAgent):
         actions = gameState.getLegalActions()
         neighbors = [gameState.generatePacmanSuccessor(a) for a in actions]
         neighborStates = [s.getPacmanPosition() for s in neighbors]
-        best = min(zip(actions, neighborStates), 
+        best = min(zip(actions, neighborStates),
                 key=lambda x: self.distances[x[1], dest])
         return best[0]
 
@@ -432,4 +446,3 @@ def combinations(n, r):
     numer = reduce(op.mul, xrange(n, n-r, -1))
     denom = reduce(op.mul, xrange(1, r+1))
     return numer / denom
-
